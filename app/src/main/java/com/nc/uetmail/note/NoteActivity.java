ca726f6053d96c38bde9dafaeae3270a77a94dea
nghichcode <nghichcode@gmail.com>
2020-08-31 16:54:42 +0700

    Stage 1.01 Swipe

--- app/src/main/java/com/nc/uetmail/note/NoteActivity.java
+++ app/src/main/java/com/nc/uetmail/note/NoteActivity.java
@@ -7,11 +7,13 @@ import android.graphics.Canvas;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,6 +23,8 @@ import android.widget.Toast;
 import com.nc.uetmail.R;
 
 import java.util.List;
+
+import com.nc.uetmail.utils.RecyclerViewSwipeDecorator;
 
 public class NoteActivity extends AppCompatActivity {
     public static final int ADD_NOTE_REQ = 1;
@@ -131,8 +135,13 @@ public class NoteActivity extends AppCompatActivity {
 
         @Override
         public void onChildDraw(@NonNull final Canvas c, @NonNull final RecyclerView recyclerView, @NonNull final RecyclerView.ViewHolder viewHolder, final float dX, final float dY, final int actionState, final boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator
+                .Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                .addBackgroundColor(ContextCompat.getColor(NoteActivity.this, R.color.colorAccent))
+                .addActionIcon(R.drawable.ic_close)
+                .create()
+                .decorate();
             super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-
         }
     };
 
